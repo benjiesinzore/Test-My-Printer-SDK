b45088092c4462141800f418d59a8c805dde3d42
burntoo <ngangamartin11@gmail.com>
2022-03-14 14:02:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ciontexprinter/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.ciontexprinter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.zxing.BarcodeFormat;
+
+import vpos.apipackage.PosApiHelper;
+import vpos.apipackage.PrintInitException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String[] MY_PERMISSIONS_STORAGE = {"android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.MOUNT_UNMOUNT_FILESYSTEMS"};
+
+    PosApiHelper posApiHelper = PosApiHelper.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, "ciontek.permission.sdcard") != 0) {
+            ActivityCompat.requestPermissions(this, MY_PERMISSIONS_STORAGE, 1);
+        }
+
+        Button print = findViewById(R.id.print);
+
+        print.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                testApiSimple();
+            }
+        });
+    }
+
+    void testApiSimple(){
+
+        Print_Thread printThread = null;
+
+        printThread = new Print_Thread();
+        printThread.start();
+
+
+    }
+
+    public class Print_Thread extends Thread {
+
+        public Print_Thread() {}
+
+        public void run() {
+            int ret = posApiHelper.PrintInit(2, 24, 24, 0x33);
+            if(ret!=0){
+                return;
+            }
+            posApiHelper.PrintStr("Print Tile\n");
+            if(ret!=0){
+                return;
+            }
+            posApiHelper.PrintStr("- - - - - - - - - - - - - - - - - - - - - - - -\n");
+            posApiHelper.PrintStr(" Print Str2 \n");
+            posApiHelper.PrintBarcode("123456789", 360, 120, BarcodeFormat.CODE_128);
+            posApiHelper.PrintBarcode("123456789", 240, 240, BarcodeFormat.QR_CODE);
+            posApiHelper.PrintStr("CODE_128 : " + "123456789" + "\n\n");
+            posApiHelper.PrintStr("QR_CODE : " + "123456789" + "\n\n");
+            posApiHelper.PrintStr(" \n");
+            int start = posApiHelper.PrintStart();
+            Log.d("Printer Start", "" +start);
+        }
+    }
+}
